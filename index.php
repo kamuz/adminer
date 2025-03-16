7819ffd10d556aacc5ae955c26ae36dfa1834ea0
Volodymyr Kamuz <v.kamuz@gmail.com>
2025-03-16 12:56:26 +0200

    Change password

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ function adminer_object() {
     include_once "plugin.php";
     include_once "login-password-less.php";
     return new AdminerPlugin(array(
-        new AdminerLoginPasswordLess(password_hash("admin", PASSWORD_DEFAULT)),
+        new AdminerLoginPasswordLess(password_hash("root", PASSWORD_DEFAULT)),
     ));
 }
 include "adminer-core.php";
